9e73bc778d708ab96cb2f339f44e17270916e4f2
Elvin Gomez <me@elvingomez.com>
2023-01-29 21:27:31 +0100

    init

---
+++ components/layouts/DarkLayout.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react"
+
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export const DarkLayout: FC<LayoutProps> = ({ children }) => {
+  return (
+    <div style={{
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        borderRadius: '5px',
+        padding: '10px'
+    }}>
+        <h3>Dark-Layout</h3>
+        {children}
+    </div>
+  )
+}
